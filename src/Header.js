28abefc1af02e024bbfd27ca8956c2b07887ec97
Manav meghpara <73911083+manavmeghpara@users.noreply.github.com>
2024-03-26 20:25:42 -0700

    user get info

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { fetchUserAttributes } from '@aws-amplify/auth';
+
 
 const Header = () => {
+
+  // Function to print access token and id token
+  const printUserAttributes = async () => {
+    try {
+      const userAttributes = await fetchUserAttributes();
+      console.log('Email:', userAttributes.email);
+    }
+    catch (e) { console.log(e); }
+  };
+
   return (
     <header style={headerStyle}>
       <div style={leftContainer}>
@@ -25,7 +37,7 @@ const Header = () => {
       <div style={rightContainer}>
         <div style={userInfoStyle}>
           <div style={userContainerStyle}>
-            <span style={usernameStyle}>Username</span>
+            <span style={usernameStyle}>userAttributes.email</span>
             <span style={accountTypeStyle}>Account Type</span>
           </div>
         </div>
